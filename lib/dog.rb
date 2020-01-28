357548d6ac5edfa332af694247ff9c26f9b0a9e9
Wesley Chen <weschen1996@gmail.com>
2020-01-28 15:59:30 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog 
+  # setter name
+  def name=(name)
+    @name = name
+  end
+  # getter name
+  def name
+    @name
+  end
+  # setter breed
+  def breed=(breed)
+    @breed = breed
+  end
+  # getter breed
+  def breed
+    @breed
+  end
+
+end
---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person
+  #setter name
+  def name= (p_name)
+    @name = p_name
+  end
+  #getter name
+  def name
+    @name
+  end
+  #setter job
+  def job= (p_job)
+    @job = p_job
+  end
+  #getter job
+  def job 
+    @job
+  end
+end
